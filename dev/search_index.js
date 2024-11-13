df6524caa65c6e9262c6888a84b9b23e5e977023
Documenter.jl <documenter@juliadocs.github.io>
2024-11-13 08:35:40 +0000

    build based on 2e38eed

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WeaveLoggers","category":"page"},{"location":"#WeaveLoggers","page":"Home","title":"WeaveLoggers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WeaveLoggers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WeaveLoggers]","category":"page"}]
+}
